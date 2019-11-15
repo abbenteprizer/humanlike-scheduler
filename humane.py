efa251351ffe717d8ffd06fed584a4d21b1489c9
abbenteprizer <abbenteprizer@gmail.com>
2019-11-15 14:40:05 +0100

    fattened normal dist

--- humane.py
+++ humane.py
@@ -2,7 +2,7 @@
 # @Author: abbenteprizer
 # @Date:   2019-11-15 13:56:58
 # @Last Modified by:   abbenteprizer
-# @Last Modified time: 2019-11-15 14:32:55
+# @Last Modified time: 2019-11-15 14:38:33
 '''
 Humane schedules tasks following a distribution that mimics 
 internet traffic. 
@@ -20,13 +20,13 @@ import matplotlib.pyplot as plt
 # def distribution()
 N = 100
 np.random.seed(1)
-dist = np.concatenate((np.random.normal(8, 1, int(0.3 * N)),
-						np.random.normal(18, 1, int(0.7 * N))))[:, np.newaxis]
+dist = np.concatenate((np.random.normal(9, 3, int(0.3 * N)),
+						np.random.normal(18, 2, int(0.3 * N))))[:, np.newaxis]
 
 dist_plot = np.linspace(0, 24, 1000)[:, np.newaxis]
 
-true_density = (0.3 * norm(8, 1).pdf(dist_plot[:, 0])
-			  + 0.7 * norm(18,1).pdf(dist_plot[:, 0]))
+true_density = (0.3 * norm(9, 3).pdf(dist_plot[:, 0])
+			  + 0.3 * norm(18,2).pdf(dist_plot[:, 0]))
 
 
 
